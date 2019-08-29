02676a550e46b53a847361f5d6a8df343c3d41f9
admin <admin>
2019-08-28 20:00:10 -0400

    Edited file 'python-runnables/push-to-remote/runnable.py'

--- python-runnables/push-to-remote/runnable.py
+++ python-runnables/push-to-remote/runnable.py
@@ -28,21 +28,27 @@ class MyRunnable(Runnable):
         # get remote key and url
         remote_url = self.config.get('remote_url')
         remote_key = self.config.get('remote_key')
+        project_key = self.project_key
+
+        # get remote key and url
+        remote_url = "http://localhost:8000"
+        remote_key = "2ruzLOctve7e66bWxhc2itf8YX6SkzOz"
+        project_key = dataiku.default_project_key()
 
 
         # get local and remote wikis for current project      
-        rp = dataikuapi.DSSClient(remote_url, api_key = remote_key).get_project(self.project_key)
-        cp = dataiku.api_client().get_project(self.project_key)
-        
+        rp = dataikuapi.DSSClient(remote_url, api_key = remote_key).get_project(project_key)
+        cp = dataiku.api_client().get_project(project_key)
+
         local_wiki  = cp.get_wiki()
         remote_wiki = rp.get_wiki()
 
         # replace or create new articles in the project wikis
-        for r_article in local_wiki.list_articles():
-            for l_article in remote_wiki.list_articles():
+        for l_article in local_wiki.list_articles():
+            for r_article in remote_wiki.list_articles():
                 if l_article.article_id == r_article.article_id:
-                    l_article.delete            
-                remote_wiki.create_article(l_article.article_id,content = l_article.get_data().get_body())
+                    r_article.delete            
+            remote_wiki.create_article(l_article.article_id,content = l_article.get_data().get_body())
                     
         return '<body>Wiki Updated on instance running at: ' + remote_url + '</body>'
         
